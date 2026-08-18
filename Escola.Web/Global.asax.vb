ccbaa6878ca60b7d73d3304903dc74743fec9c01
Diego Moreira <diego.moreira529@gmail.com>
2026-08-17 22:24:33 -0300

    feat: permite o desenvolvedor configuar um usuário para testes

--- Escola.Web/Global.asax.vb
+++ Escola.Web/Global.asax.vb
@@ -1,8 +1,43 @@
 ﻿
+Imports Escola.Data
+
 Public Class Global_asax
+
     Inherits HttpApplication
 
+    ' Permite que o desenvolvedor consiga fazer logout
+    Private Property TentouLogarComUsuarioTeste As Boolean = False
+
     Sub Application_Start(sender As Object, e As EventArgs)
-        ' Fires when the application is started
+
     End Sub
+
+    Private Sub Session_Start(sender As Object, e As EventArgs)
+
+        LogarComUsuarioTeste()
+
+    End Sub
+
+    Private Sub LogarComUsuarioTeste()
+
+        If Not TentouLogarComUsuarioTeste AndAlso Ambiente.EhDesenvolvimento Then
+            TentouLogarComUsuarioTeste = True
+
+            Try
+                Dim repo = New UsuarioRepository
+                Dim usuarioTeste = repo.BuscarPorNomeDeUsuario(ConfigurationManager.AppSettings("UsuarioTeste"))
+                Dim usuarioLogado = New UsuarioLogado With {
+                    .Id = usuarioTeste.Id,
+                    .NomeDeUsuario = usuarioTeste.NomeDeUsuario
+                }
+
+                AutenticacaoUtils.Autenticar(Session, usuarioLogado)
+            Catch ex As Exception
+                Debug.WriteLine("Erro ao tentar autenticar com usuário configurado.")
+                Debug.WriteLine(ex)
+            End Try
+        End If
+
+    End Sub
+
 End Class
---
+++ Escola.Web/Utils/Ambiente.vb
@@ -0,0 +1,6 @@
+﻿Public Class Ambiente
+
+    Public Shared Property EhDesenvolvimento As Boolean = ConfigurationManager.AppSettings("Ambiente") = "Desenvolvimento"
+    Public Shared Property EhProducao As Boolean = ConfigurationManager.AppSettings("Ambiente") = "Producao"
+
+End Class
